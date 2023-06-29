b4379c0f603a70b4db222c2ebde5b76679f4411b
AmdjedBelgacem <amgedbelgacem@gmail.com>
2023-06-30 01:02:22 +0300

    Added IntroPic for first Step

---
+++ app/IntroPic.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles/styles.css";
+function IntroPic() {
+  return (
+    <div className="container">
+      <img
+        src="https://assets.api.uizard.io/api/cdn/stream/a8ce660d-47da-404a-b3b3-63ca6970ddcf.png"
+        className="introPic"
+      />
+    </div>
+  );
+}
+
+export default IntroPic;
