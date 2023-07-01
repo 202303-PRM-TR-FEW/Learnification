11eeb94f1b1d19de3aab107d48564324c977fd61
Furkan Cengiz <muhammedcengiz1@gmail.com>
2023-07-02 00:34:38 +0300

    add: Routing for all pages
    Route Group named (content-pages) added to use layout for required pages
    app/page.jsx redirets users to /get-started
    Fixes #23

---
+++ app/(content-pages)/home/page.jsx
@@ -0,0 +1,11 @@
+"use client"
+
+import React, { useEffect } from 'react'
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Home</h1>
+        </div>
+    )
+}
---
+++ app/get-started/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import BrandIcon from '../components/BrandIcon'
+import TitleAndParagraph from '../components/TitleAndParagraph'
+
+export default function Welcome() {
+    return (
+        <main>
+            <BrandIcon />
+            <TitleAndParagraph />
+        </main>
+    )
+}
