3e8a1f3b3b464ec35226be3a1419cdbcea034a31
AmdjedBelgacem <amgedbelgacem@gmail.com>
2023-07-02 13:44:55 +0300

    Only Tailwind Styling

--- app/IntroPic.jsx
+++ app/IntroPic.jsx
@@ -5,7 +5,7 @@ function IntroPic() {
     <div className="container">
       <img
         src="https://assets.api.uizard.io/api/cdn/stream/a8ce660d-47da-404a-b3b3-63ca6970ddcf.png"
-        className="introPic"
+        className="introPic w-full bg-white h-[38rem] rounded-[50px]"
       />
     </div>
   );
