0ce03154adc383a2bb36e7aa893fe3dca55b4b80
Furkan Cengiz <muhammedcengiz1@gmail.com>
2023-07-05 12:08:56 +0300

    fix: image optimization in Tutor.

--- app/Components/Tutor.jsx
+++ app/Components/Tutor.jsx
@@ -9,7 +9,7 @@ export default function Tutor({ tutor }) {
     return (
         <>
             <div className="w-10 h-10 rounded-full overflow-hidden">
-                <Image src={tutor?.imageUrl} alt={tutor?.name} width={60} height={60} sizes='100vw' />
+                <Image src={tutor?.imageUrl} alt={tutor?.name} width={60} height={60} />
             </div>
             <div className="ml-2 pr-2">
                 <p>
