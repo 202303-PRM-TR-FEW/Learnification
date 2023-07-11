53325038f1219d85fac1deca8f65bf9faa39bbd4
Furkan <84590614+4Furki4@users.noreply.github.com>
2023-07-12 01:55:42 +0300

    add: title and statistic icon, and initial translations

--- app/[locale]/(content-pages)/my-learning/page.jsx
+++ app/[locale]/(content-pages)/my-learning/page.jsx
@@ -1,7 +1,28 @@
+import Link from 'next/link'
 import React from 'react'
-
+import Icons from '../../../Components/Icons.jsx'
+import { useTranslations } from 'next-intl'
 export default function MyLearning() {
+    const t = useTranslations('MyLearning')
     return (
-        <div>MyLearning</div>
+        <main className='w-full'>
+            <div className='flex justify-between w-1/2'>
+                <h1>
+                    {t('title')}
+                </h1>
+                <Link href='/statistics'>
+                    <p className='flex gap-2 lg:gap-6'>
+                        <span>
+                            <Icons.StatisticsIcon />
+                        </span>
+                        <span>
+                            {t('statistics')}
+                        </span>
+                    </p>
+                </Link>
+                {/* <MyLearningCard /> */}
+            </div>
+            <div></div>
+        </main>
     )
 }
