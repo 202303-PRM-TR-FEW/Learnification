bda6d1d4964eb8a279642355b9ee5e7fabaa6740
Tugba Esat <tugbaesat007@gmail.com>
2023-07-13 16:00:02 +0300

    number of videos is displayed and design is edited

--- app/Components/CourseOverview.jsx
+++ app/Components/CourseOverview.jsx
@@ -1,12 +1,12 @@
 "use client";
-import { useState } from "react"; 
+import { useState } from "react";
 import CourseDetails from "./CourseDetails";
 import Icons from "./Icons";
 import { useTranslations } from "next-intl";
 
 export default function CourseOverview() {
   const lessons = CourseDetails();
-  const t = useTranslations("CourseOverview")
+  const t = useTranslations("CourseOverview");
 
   const [openLessons, setOpenLessons] = useState({});
 
@@ -19,43 +19,60 @@ export default function CourseOverview() {
 
   return (
     <ul className="text-gray-600 shadow rounded-lg p-2">
-        <h1 className="text-2xl font-bold">{t("title")}</h1>
+      <h1 className="text-2xl font-bold">{t("title")}</h1>
       {lessons.map((lesson) => (
         <li
           key={lesson.id}
-          className="flex flex-col shadow rounded-lg p-2 m-6 overflow-hidden"
+          className="flex flex-col shadow rounded-3xl p-2 my-3 mx-6 overflow-hidden"
           onClick={() => handleLessonClick(lesson.id)}
         >
-          <div className="flex justify-between">
-           
-            <h3 className="text-black flex"> <Icons.CheckIcon/> <span className="pl-2"> {lesson.id > 0 && `${lesson.id}. `}
-                        {lesson.title}</span></h3>
+          <div className="flex justify-between py-3 px-6">
+            <h3 className="text-black flex font-bold">
+              <Icons.CheckIcon />
+              <span className="pl-2">
+                {lesson.id > 0 && `${lesson.id}. `}
+                {lesson.title}
+              </span>
+            </h3>
             {openLessons[lesson.id] ? (
-              <Icons.DownArrowIcon />
+              <div className="relative top-5">
+                <Icons.DownArrowIcon />
+              </div>
             ) : (
-              <Icons.RightArrowIcon />
+              <div className="relative top-5">
+                <Icons.RightArrowIcon />
+              </div>
             )}
           </div>
-          <div className="overflow-auto max-h-56">
-            {openLessons[lesson.id] && (
+          {openLessons[lesson.id] ? null : (
+            <p className="pl-6 pb-3">
+              {lesson.videos.length > 1
+                ? `${lesson.videos.length} videos`
+                : `${lesson.videos.length} video`}
+            </p>
+          )}
+          {openLessons[lesson.id] && (
+            <div className="overflow-auto max-h-56">
               <ul className="">
                 {lesson.videos.map((video) => (
-                  <li key={video.id} className="p-1 my-4">
+                  <li key={video.id} className="pl-6 my-4">
                     <div>
-                      <h4 className="flex"><Icons.CheckIcon width={14} height={14}/> <span className="pl-2">
-                        {lesson.id > 0 && `${video.id}. `}
-                        {video.title}
-                        </span> </h4>
+                      <h4 className="flex">
+                        <Icons.CheckIcon width={14} height={14} />
+                        <span className="pl-2">
+                          {lesson.id > 0 && `${video.id}. `}
+                          {video.title}
+                        </span>
+                      </h4>
                       <p className="py-1">{video.duration}</p>
                     </div>
                   </li>
                 ))}
               </ul>
-            )}
-          </div>
+            </div>
+          )}
         </li>
       ))}
     </ul>
   );
 }
-
