969b53a7d5e8a791852cedb182fe592d3f692a25
Furkan <84590614+4Furki4@users.noreply.github.com>
2023-07-14 20:22:36 +0300

    add: next auth providers, credentials, signin page
    my courses part in home page is being protected using session.

--- app/[locale]/(content-pages)/home/page.jsx
+++ app/[locale]/(content-pages)/home/page.jsx
@@ -2,14 +2,28 @@ import React from "react";
 import Categories from "../../../Components/HomePage/Categories";
 import FeaturedCourses from "../../../Components/HomePage/FeaturedCourses";
 import MyLearning from "../../../Components/MyLearning";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { signIn } from "next-auth/react";
+import LearnUButton from "@/app/Components/LearnUButton";
+import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession()
+  // if (!session) redirect('api/auth/signin?callback=home') to redirect user wo seeing the page
   return (
     <>
       <div className="basis-full mt-2 px-6 sm:px-12 pb-20 bg-neutral-100">
         <FeaturedCourses />
         <Categories />
-        <MyLearning />
+        {!session ? (
+          <div className="flex">
+            <Link className="mt-2 m-auto" href={"/api/auth/signin?callback=home"}>
+              <LearnUButton text={"Sign in to see your courses"} />
+            </Link>
+          </div>
+        ) : <MyLearning />
+        }
       </div>
     </>
   );
--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,22 +1,40 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
+import GitHubProvider from 'next-auth/providers/github'
+import Credentials from 'next-auth/providers/credentials'
 const handler = NextAuth({
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET
+        }),
+        GitHubProvider({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET
+        }),
+        Credentials({
+            name:"Credentials",
+            credentials:{
+                email: {label:"Email", placeholder:"Please enter your email", type:"email",},
+                password: {label:"Password", placeholder:"Please enter your password",type:"password" }
+            },
+            authorize(credentials, req){
+                // query if the user exists
+                const user = { id: "1", name: "Furkan Cengiz", email: "muhammedcengiz1@gmail.com" }
+                console.log(credentials);
+                if(user.email === credentials.email)
+                return true
+                else return null
+            }
         })
     ],
     callbacks:{
         signIn({ user, account, profile, email, credentials }){
-            // check if user is allowed to sign in
-            console.log("signing in...");
+            // check if user is allowed to sign in and save their data to db
+            console.log(credentials);
             return true
         }
     },
-    pages:{
-        signIn: '/auth/signin',
-    }
 })
 
 
