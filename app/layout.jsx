19cfd7cdae2e126fb208c8aef2c7af9dd4567a8a
Furkan <84590614+4Furki4@users.noreply.github.com>
2023-07-17 19:18:28 +0300

    fix: metadata causes build error removed

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,12 +4,6 @@ import './globals.css'
 import { Rubik } from 'next/font/google'
 
 const rubik = Rubik({ subsets: ['latin'] })
-
-export const metadata = {
-  title: 'LearnU',
-  description: 'LearnU is a platform for learning and teaching online.'
-}
-
 export default function RootLayout({ children, session }) {
   return (
     <html lang="en">
