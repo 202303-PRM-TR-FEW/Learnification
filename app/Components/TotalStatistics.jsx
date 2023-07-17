163f333d4195d45824831e8c481e0b1bf1b21809
Ebla Ekerdiker <eblaekerdiker@gmail.com>
2023-07-17 22:44:18 +0300

    Translate pages updated, card width changed, card colors changed.

--- app/Components/TotalStatistics.jsx
+++ app/Components/TotalStatistics.jsx
@@ -1,28 +1,35 @@
 import React from "react";
 import Icons from "./Icons";
+import { useTranslations } from "next-intl";
+
 
 export default function TotalStatistics() {
+  const t = useTranslations("Profile")
+
   const totalStatistics = [
-    { count: 3, label: 'FINISHED COURSES', icon: Icons.CheckIconProfile, bgColor: 'bg-blue-200' },
-    { count: 56, label: 'HOURS LEARNED', icon: Icons.HourglassIcon, bgColor: 'bg-amber-200' },
-    { count: 7, label: 'SKILLS ACHIEVED', icon: Icons.SkillsIcon, bgColor: 'bg-green-200' }
+    { count: 3, label: t('FINISHED COURSES'), icon: Icons.CheckIconProfile, bgColor: 'bg-light-blue' },
+    { count: 56, label: t('HOURS LEARNED'), icon: Icons.HourglassIcon, bgColor: 'bg-light-orange' },
+    { count: 7, label: t('SKILLS ACHIEVED'), icon: Icons.SkillsIcon, bgColor: 'bg-light-green' }
   ];
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 md:p-4 lg:p-4 gap-4 ml-8 mt-20 mr-6">
+    <div>
+      <h1 className="font-semibold ml-8 mt-16 mb-4 md:ml-12 md:mb-2 text-lg lg:text-2xl md:text-xl">{t("title")}</h1>
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 md:p-4 lg:p-4 gap-4 ml-8 mr-6">
       {totalStatistics.map((statistic, index) => (
         <div key={index} className="w-full h-full p-1 bg-white rounded-3xl overflow-hidden shadow-md">
-            <div className="mt-1 mb-3 ml-1 mr-1">
-                <div className={`h-16 md:w-32 md:ml-1 rounded-2xl flex items-center justify-center ${statistic.bgColor}`}>
+            <div className="mt-1 mb-3 mr-1 ml-1">
+                <div className={`h-20 md:w-42 lg:w-44 rounded-2xl flex items-center justify-center ${statistic.bgColor}`}>
                     {statistic.icon && <statistic.icon />}
                 </div>
                 <div className="flex flex-col items-center justify-center mt-6 mb-1 font-semibold text-sm">
                     <h6 className="text-gray-600">{statistic.label}</h6>
-                    <p className="mt-1">{statistic.count}</p>
+                    <p className="mt-1 text-base">{statistic.count}</p>
                 </div>
             </div>
         </div>
       ))}
+    </div>
     </div>
   );
 }
