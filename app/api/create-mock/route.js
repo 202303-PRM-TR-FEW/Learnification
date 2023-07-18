ad89c61ecfe7382273e50e0b7895f72b4fd31b4a
Furkan <84590614+4Furki4@users.noreply.github.com>
2023-07-18 18:11:52 +0300

    fix-build: missing new keyword added...

--- app/api/create-mock/route.js
+++ app/api/create-mock/route.js
@@ -5,7 +5,7 @@ import { NextResponse } from "next/server";
 export async function GET(){
     await connectToDb();
     await createSampleData();
-    return NextResponse(
+    return new NextResponse(
         {
             status: 200,
             body: "Database populated successfully"
