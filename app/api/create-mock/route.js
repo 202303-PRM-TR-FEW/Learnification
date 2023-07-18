e902940a67a7c221623488ac1d7406b4c4965f55
Furkan <84590614+4Furki4@users.noreply.github.com>
2023-07-18 18:10:06 +0300

    fix-build: api/create-mock api response added

--- app/api/create-mock/route.js
+++ app/api/create-mock/route.js
@@ -1,7 +1,14 @@
 import createSampleData from "@/population/populateDatabase";
 import { connectToDb } from "@/utils/database";
+import { NextResponse } from "next/server";
 
 export async function GET(){
     await connectToDb();
     await createSampleData();
+    return NextResponse(
+        {
+            status: 200,
+            body: "Database populated successfully"
+        }
+    )
 }
