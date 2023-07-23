57197967e6b88156f72f58f106cca8012b71296e
Furkan <84590614+4Furki4@users.noreply.github.com>
2023-07-23 11:28:43 +0300

    add: buttons to signIn with callbackUrls
    the page user was in is stored as a queryParam and passed into signIn method so that user can be navigated to the page when successfully signed in for seamless experience.

--- app/Components/SignInUp.jsx
+++ app/Components/SignInUp.jsx
@@ -3,33 +3,42 @@ import React, { useState } from "react";
 import Link from "next/link";
 import { signIn } from 'next-auth/react';
 import LearnUButton from "./LearnUButton";
+import { useSearchParams } from "next/navigation";
 
 export default function SignInUp() {
   const [isLoginFormVisible, setLoginFormVisible] = useState(true);
   const [credentials, setCredentials] = useState({ email: '', password: '' });
+  const searchParams = useSearchParams()
+  const callbackUrl = searchParams.get('callbackUrl')
   const handleToggleForm = () => {
     setLoginFormVisible(!isLoginFormVisible);
   };
 
-  // const handleSignInWithProvider = async (provider) => {
-  //   await signIn(provider, { callbackUrl: '@/app/api/auth/[...nextauth]/route.js' });
-  // };
   function handleSubmit(e) {
     e.preventDefault();
-    signIn('credentials', { ...credentials })
+    signIn('credentials', { callbackUrl: callbackUrl, ...credentials })
   }
 
   return (
     <div className="min-h-screen flex items-center justify-center mx-2 md:mx-0">
       <div className="bg-white bg-opacity-50 backdrop-blur-xl p-8 rounded-2xl shadow-xl">
+        <div className="flex flex-col sm:flex-row justify-around">
+          <div onClick={() => signIn('github', { callbackUrl: callbackUrl })}>
+            <LearnUButton className={"my-4"} text="Sign In with GitHub" />
+          </div>
+          <div onClick={() => signIn('google', { callbackUrl: callbackUrl })}>
+            <LearnUButton className={"my-4"} text="Sign In with Google" />
+          </div>
+        </div>
         {isLoginFormVisible ? (
           <form onSubmit={(e) => handleSubmit(e)}>
             <h2 className="text-sky-500 font-semibold text-2xl pb-5">Login</h2>
+
             <input
               value={credentials.email}
               onChange={(e) => setCredentials({ ...credentials, email: e.target.value })}
-              type="text"
-              placeholder="Username"
+              type="email"
+              placeholder="Email"
               required
               className="w-full py-2 px-1 text-gray-400 mb-8 border-b border-gray-500 outline-none bg-transparent"
             />
@@ -71,7 +80,7 @@ export default function SignInUp() {
               className="w-full py-2 px-1 text-gray-400 mb-8 border-b border-gray-500 outline-none bg-transparent"
             />
 
-            <LearnUButton text="Sign Up" onClick={() => handleSignInWithProvider('credentials')} />
+            <LearnUButton text="Sign Up" />
             <p className="message text-gray-400 text-sm mt-6">
               Already registered?{' '}
               <Link href="#" onClick={handleToggleForm} className="text-blue-500">
