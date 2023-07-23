a82042dcb7fbf7398d3373616a6ed69bb09363f2
Tugba Esat <tugbaesat007@gmail.com>
2023-07-23 18:02:57 +0300

    header is created

--- app/[locale]/(content-pages)/layout.jsx
+++ app/[locale]/(content-pages)/layout.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
-import SideBar from '../../Components/SideBar'
+import React from "react";
+import SideBar from "../../Components/SideBar";
+import Header from "@/app/Components/Header";
 export default function Layout({ children }) {
-    return (
-        <div className='bg-white-smoke min-h-screen md:flex md:items-start
-            md:pr-[50px] lg:pr-[100px]'>
-            <SideBar />
-            {children}
+  return (
+    <div className="flex flex-col">
+        <div className="bg-white-smoke">
+        <Header/>
         </div>
-    )
+      <div
+        className="bg-white-smoke min-h-screen md:flex md:items-start
+        md:pr-[50px] lg:pr-[100px]"
+      >
+        <SideBar />
+        {children}
+      </div>
+    </div>
+  );
 }
--- app/[locale]/(content-pages)/search/page.jsx
+++ app/[locale]/(content-pages)/search/page.jsx
@@ -5,7 +5,7 @@ import RecommendedForYou from "@/app/Components/SearchPage/RecommendedForYou";
 
 export default function Search() {
   return (
-    <div className="basis-full mt-2 px-6 sm:px-12 pb-20 bg-neutral-100">
+    <div className="basis-full mt-2 px-6 sm:px-12 pb-20 bg-neutral-100">  
       <SearchFormPartOne/>
       <SearchFormPartTwo/>
       <RecommendedForYou />
