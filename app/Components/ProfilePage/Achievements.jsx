69bf17d05ff810605e0f0ff35c7a9b2ba72e937d
Ebla Ekerdiker <eblaekerdiker@gmail.com>
2023-07-24 16:44:00 +0300

    Profile picture rounded for all page sizes. Achievements added.

---
+++ app/Components/ProfilePage/Achievements.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Icons from "./../Icons";
+import { useTranslations } from "next-intl";
+
+export default function Achievements() {
+  const t = useTranslations("Profile");
+
+  const achievements = [
+    { achievement: 'Commited Learner', icon: Icons.HourglassIcon, bgColor: 'bg-light-orange', counter: 2, goal: 3, description: "Reach a 3 days streak"},
+    { achievement: 'Point Collector', icon: Icons.SkillsIcon, bgColor: 'bg-light-green', counter: 1200, goal: 3000, description: "Earn 1800 more points"}
+  ];
+  return (
+    <div className="rounded-3xl mt-4 p-4">
+        <h1 className="text-lg lg:text-2xl font-semibold mb-3">Achievements</h1>
+      {achievements.map((achievement, index) => (
+        <div key={index} className="rounded-3xl shadow-md p-2 mb-4 flex items-center">
+          <div className={`h-28 w-32 rounded-2xl flex items-center justify-center ${achievement.bgColor}`}>
+            {achievement.icon && <achievement.icon />}
+          </div>
+          <div className="ml-4 flex flex-col flex-grow gap-y-2">
+            <div className="flex justify-between">
+              <h1 className="text-md font-semibold mt-0 text-2xl">{achievement.achievement}</h1>
+              <p className="hidden sm:block text-gray-400">{`${achievement.counter}/${achievement.goal}`}</p>
+            </div>
+            <div className="relative h-5 sm:h-3 bg-gray-200 rounded" key={index}>
+              <div className={`absolute top-0 left-0 h-full rounded-full flex justify-end ${achievement.bgColor}`} style={{width:`${Math.round(achievement.counter*100/achievement.goal)}%`}}>
+                <span className="sm:hidden text-gray-400 text-xs">{`${achievement.counter}/${achievement.goal}`}</span>
+              </div>
+            </div>
+            <p className="text-gray-400">{achievement.description}</p>
+          </div>
+        </div>
+      ))}   
+    </div>
+  );
+}
--- app/[locale]/(content-pages)/profile/page.jsx
+++ app/[locale]/(content-pages)/profile/page.jsx
@@ -3,20 +3,24 @@ import UserInfo from "../../../Components/ProfilePage/UserInfo";
 import TotalStatistics from '@/app/Components/ProfilePage/TotalStatistics';
 import FriendsComponent from '@/app/Components/ProfilePage/FriendsComponent'
 import LearnUButton from '@/app/Components/LearnUButton'
+import Achievements from '@/app/Components/ProfilePage/Achievements';
 
 export default function Profile() {
     return (
         <div className='flex flex-col items-center lg:flex-row pt-16 w-full gap-y-16'>
-            <div className="w-full lg:w-3/5 flex flex-col items-center">
+            <div className="w-full lg:w-3/5 flex flex-col items-center mr-6">
                 <UserInfo/>
                 <div className='w-full'>
                     <TotalStatistics/>
                 </div>
+                <div className='w-full ml-10'>
+                    <Achievements/>
+                </div>
             </div>
-            <div className='w-full lg:w-2/5 md:max-w-none px-2 md:px-6 max-w-md '>
+            <div className='w-full lg:w-2/5 md:max-w-none px-2 md:px-6 max-w-md'>
                 <FriendsComponent myButton="addDelete"/>
                 <FriendsComponent/>
-                <div className="mt-12 flex flex-col sm:flex-row gap-4">
+                <div className="mt-12 flex flex-col sm:flex-row itms-center justify-center gap-4">
                     <LearnUButton text="FIND FRIENDS" bgColor="white"/>
                     <LearnUButton text="INVITE FRIENDS"/>
                 </div>
