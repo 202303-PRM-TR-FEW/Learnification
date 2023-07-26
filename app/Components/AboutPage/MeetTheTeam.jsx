5904744acaad79b42ff8481f3da25a2a0cbbbec7
Tugba Esat <tugbaesat007@gmail.com>
2023-07-26 13:32:20 +0300

    member name mt removed

--- app/Components/AboutPage/MeetTheTeam.jsx
+++ app/Components/AboutPage/MeetTheTeam.jsx
@@ -10,19 +10,31 @@ const MeetTheTeam = () => {
     <div className="mt-8">
       <ul className="flex flex-wrap justify-center gap-16 bg-blue-500 rounded-[50px] ">
         {members.map((member) => (
-          <li key={member.id} className="profile-card relative w-56 h-56 bg-white p-8 rounded-full m-5  ">
+          <li
+            key={member.id}
+            className="profile-card relative w-56 h-56 bg-white p-8 rounded-full m-5  "
+          >
             <div className="relative w-full h-full">
-              <Image src={member.image} className="profile-image object-fill w-full rounded-full z-[99] " alt="Team member's images" />
+              <Image
+                src={member.image}
+                className="profile-image object-fill w-full rounded-full z-[99] "
+                alt="Team member's images"
+              />
             </div>
-            <div className="caption text-center opacity-0 ">
-              <h3 className="font-bold text-blue-500 mt-4 mb-2">{member.name}</h3>
+            <div className="caption text-center opacity-0">
+              <h3 className="font-bold text-blue-500 mb-2">
+                {member.name}
+              </h3>
               <p className="mb-2">{member.title}</p>
-              <div className="social-links flex justify-center gap-2">
-              <a href={member.links.github}><Icons.GithubIcon/></a>
-              <a href={member.links.linkedin}> <Icons.LinkedinIcon/> </a>
+              <div className="flex justify-center gap-2">
+                <a href={member.links.github}>
+                  <Icons.GithubIcon />
+                </a>
+                <a href={member.links.linkedin}>
+                  <Icons.LinkedinIcon />
+                </a>
+              </div>
             </div>
-            </div>
-            
           </li>
         ))}
       </ul>
