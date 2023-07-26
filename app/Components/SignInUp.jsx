da411bd2c4fb4cd13905ae738354257b3a126bac
Furkan Cengiz <muhammedcengiz1@gmail.com>
2023-07-26 19:09:56 +0300

    fix: have removed unnecessary libraries
    The global.css file was not functioning properly due to unnecessary spaces in the hover section.
    
    unnecessary imports removed.

--- app/Components/SignInUp.jsx
+++ app/Components/SignInUp.jsx
@@ -1,6 +1,5 @@
 'use client'
 import React, { useState } from "react";
-import Link from "next/link";
 import { signIn } from 'next-auth/react';
 import LearnUButton from "./LearnUButton";
 import { useSearchParams } from "next/navigation";
