9fcc33e2f2c5cef9d76bce1fa2f7ab2148db9f8f
Amdjed Belgacem <45467302+AmdjedBelgacem@users.noreply.github.com>
2023-07-27 19:49:05 +0300

    Added Missing Translations

--- app/Components/StatisticsPage/MyPerformance.jsx
+++ app/Components/StatisticsPage/MyPerformance.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Icons from "./Icons";
+import Icons from "../Icons";
 import { useTranslations } from "next-intl";
 
 export default function MyPerformance() {
@@ -13,15 +13,15 @@ export default function MyPerformance() {
 
   return (
     <div>
-      <h1 className="font-semibold ml-8 mt-16 mb-4 md:ml-12 md:mb-2 text-lg lg:text-2xl md:text-xl">{t("title")}</h1>
-      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 md:p-4 lg:p-4 gap-4 ml-8 mr-6">
+      <h1 className="font-semibold ml-2 pb-4 text-2xl">{t("Title")}</h1>
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 px-4">
         {myPerformance.map((performance, index) => (
-          <div key={index} className="w-full h-full p-1 bg-white rounded-3xl overflow-hidden shadow-md">
+          <div key={index} className="w-full p-1 bg-white rounded-3xl overflow-hidden shadow-md">
             <div className="mt-1 mb-3 mr-1 ml-1">
-              <div className={`h-20 md:w-36 lg:w-44 rounded-2xl flex items-center justify-center ${performance.bgColor}`}>
+              <div className={`h-20 w-full rounded-2xl flex items-center justify-center ${performance.bgColor}`}>
                 {performance.icon && <performance.icon />}
               </div>
-              <div className="flex flex-col items-center justify-center mt-6 mb-1 font-semibold text-sm">
+              <div className="flex flex-col items-center justify-center mt-6 mb-1 font-semibold text-sm text-center">
                 <h6 className="text-gray-600">{performance.label}</h6>
                 <p className="mt-1 text-base">{performance.count}</p>
               </div>
