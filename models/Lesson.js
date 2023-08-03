62545a28f90bad8fc511646947da1f82bd0f6b6a
Amdjed Belgacem <45467302+AmdjedBelgacem@users.noreply.github.com>
2023-08-03 17:45:38 +0300

    removed sections out of lesson and provided right durations in hours and minutes

--- models/Lesson.js
+++ models/Lesson.js
@@ -1,11 +1,6 @@
 import { Schema, models, model } from "mongoose";
 
 const lessonSchema = new Schema({
-  sections: {
-    type: Schema.Types.ObjectId,
-    ref: "Section",
-    required: true,
-  },
   name: {
     type: String,
     required: true,
