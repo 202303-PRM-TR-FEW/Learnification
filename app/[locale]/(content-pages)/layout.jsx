91e2c29f7e8d8755c22f367bdcb4564c6421a8be
Tugba Esat <tugbaesat007@gmail.com>
2023-08-06 16:23:57 +0300

    loading page testing

--- app/[locale]/(content-pages)/layout.jsx
+++ app/[locale]/(content-pages)/layout.jsx
@@ -1,18 +1,29 @@
-import React from "react";
-import SideBar from "../../Components/SideBar";
+'use client'
+import { useState, useEffect } from 'react';
 import Header from "@/app/Components/Header";
+import SideBar from "@/app/Components/SideBar";
+import Loading from '@/app/Components/LoadingPage/Loading';
+
 export default function Layout({ children }) {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000); 
+  }, []);
+
   return (
-    <div className="flex flex-col">
+    <div>
+    {isLoading ? <Loading/> :  <div className="flex flex-col">
       <div className="bg-white-smoke lg:hidden">
-        <Header />
+        <Header/>
       </div>
-      <div
-        className="bg-white-smoke min-h-screen md:flex md:items-start"
-      >
-        <SideBar />
+      <div className="bg-white-smoke min-h-screen md:flex md:items-start">
+        <SideBar/>
         {children}
       </div>
+    </div>}
     </div>
   );
 }
--- app/[locale]/layout.jsx
+++ app/[locale]/layout.jsx
@@ -1,32 +1,32 @@
-import { NextIntlClientProvider, useLocale } from "next-intl"
-import { notFound } from "next/navigation"
-import '../globals.css'
-import { Rubik } from 'next/font/google'
-const rubik = Rubik({ subsets: ['latin'] })
+import { NextIntlClientProvider, useLocale } from "next-intl";
+import { notFound } from "next/navigation";
+import "../globals.css";
+import { Rubik } from "next/font/google";
+const rubik = Rubik({ subsets: ["latin"] });
 
 export const metadata = {
-    title: 'LearnU',
-    description: 'LearnU is a platform for learning and teaching online.'
-}
+  title: "LearnU",
+  description: "LearnU is a platform for learning and teaching online.",
+};
 
 export default async function LocaleLayout({ children, params }) {
-    const locale = useLocale()
-    if (params.locale !== locale) {
-        return notFound()
-    }
-    let messages;
-    try {
-        messages = (await import(`../../messages/${locale}.json`)).default;
-    } catch (error) {
-        notFound();
-    }
-    return (
-        <html lang="{locale}">
-            <body className={rubik.className}>
-                <NextIntlClientProvider messages={messages} locale={locale}>
-                    {children}
-                </NextIntlClientProvider>
-            </body>
-        </html>
-    )
+  const locale = useLocale();
+  if (params.locale !== locale) {
+    return notFound();
+  }
+  let messages;
+  try {
+    messages = (await import(`../../messages/${locale}.json`)).default;
+  } catch (error) {
+    notFound();
+  }
+  return (
+    <html lang="{locale}">
+      <body className={rubik.className}>
+        <NextIntlClientProvider messages={messages} locale={locale}>
+          {children}
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
 }
