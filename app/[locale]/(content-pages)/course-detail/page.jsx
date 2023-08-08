17e0f3c7a76b082607d45acec73e358cbebf98f0
Tugba Esat <tugbaesat007@gmail.com>
2023-08-08 15:31:13 +0300

    control checkpoint

--- app/[locale]/(content-pages)/course-detail/page.jsx
+++ app/[locale]/(content-pages)/course-detail/page.jsx
@@ -7,7 +7,7 @@ import Image from "next/image";
 import CourseOverview from "@/app/Components/CourseOverview";
 import LearnUButton from "@/app/Components/LearnUButton";
 
-function CourseViewImage({ imgUrl, courseId }) {
+function CourseViewImage({ imgUrl }) {
   return (
     <div className="relative w-full h-[250px] lg:h-[350px] xl:h-[400px] 2xl:h-[500px] rounded-2xl">
       <Image
--- app/[locale]/(content-pages)/saved/page.jsx
+++ app/[locale]/(content-pages)/saved/page.jsx
@@ -1,39 +1,43 @@
-"use client"
-import CourseView from '@/app/Components/CourseView';
-import LearnUButton from '@/app/Components/LearnUButton';
-import CoursePreview from '@/app/Components/CoursePreview';
-import { useTranslations } from 'next-intl';
-import { redirect, usePathname, useRouter, useSearchParams } from 'next/navigation';
-import React, { useCallback, useEffect, useRef } from 'react'
-import { useState } from 'react';
-import Image from 'next/image';
-import { useSession } from 'next-auth/react';
+"use client";
+import CourseView from "@/app/Components/CourseView";
+import LearnUButton from "@/app/Components/LearnUButton";
+import CoursePreview from "@/app/Components/CoursePreview";
+import { useTranslations } from "next-intl";
+import {
+  redirect,
+  usePathname,
+  useRouter,
+  useSearchParams,
+} from "next/navigation";
+import React, { useCallback, useEffect, useRef } from "react";
+import { useState } from "react";
+import Image from "next/image";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
 
 function CourseViewImage({ imgUrl }) {
-    const session = useSession()
-    if (!session.data) redirect('/sign-in?callbackUrl=/saved')
-    const t = useTranslations("SavedCourses")
-    return (
-        <div
-            className='relative w-full h-[250px] lg:min-h-[200px] basis-full rounded-2xl'
-        >
-            <Image
-                src={imgUrl}
-                width={1920}
-                height={1080}
-                alt="Course Image"
-                className="w-full h-full object-cover rounded-2xl"
-                sizes='(min-width: 1280px) 80vw, (min-width: 1024px) 50vw, (min-width: 768px) 80vw, 100vw'
-            />
-            <div className='text-white-smoke text-center absolute z-50 inset-1 m-auto max-w-max max-h-max'>
-                <button
-                    className='relative left-2 w-[0px] h-[0px]
+  const session = useSession();
+  if (!session.data) redirect("/sign-in?callbackUrl=/saved");
+  const t = useTranslations("SavedCourses");
+  return (
+    <div className="relative w-full h-[250px] lg:min-h-[200px] basis-full rounded-2xl">
+      <Image
+        src={imgUrl}
+        width={1920}
+        height={1080}
+        alt="Course Image"
+        className="w-full h-full object-cover rounded-2xl"
+        sizes="(min-width: 1280px) 80vw, (min-width: 1024px) 50vw, (min-width: 768px) 80vw, 100vw"
+      />
+      <div className="text-white-smoke text-center absolute z-50 inset-1 m-auto max-w-max max-h-max">
+        <button
+          className="relative left-2 w-[0px] h-[0px]
                     border-solid border-[15px] border-l-[30px]
                     border-b-transparent border-t-transparent
                     border-r-transparent border-l-white
                     transition-all duration-300
                     hover:border-l-primary-blue
-                    '
+                    "
         ></button>
         <p className="text-xl text-center block font-semibold mt-1">
           {t("Play Preview")}
@@ -219,63 +223,96 @@ export default function Saved() {
     setExpandedCourseIndex(defaultIndex);
   }, [searchParams.get("courseIndex")]);
 
-  const handlePreviewClick = useCallback(() => {
-    const route = "course-detail?courseIndex=" + expandedCourseIndex;
-    router.push(route);
-  }, [expandedCourseIndex]);
+  // const handlePreviewClick = useCallback(() => {
+  //   const route = "course-detail?courseIndex=" + expandedCourseIndex;
+  //   router.push(route);
+  // }, [expandedCourseIndex]);
 
-    return (
-        <main className='w-full md:pl-12 px-[4%] md:px-[2%] lg:px-0'>
-            <div className='flex min-w-full gap-4 max-md:pb-20'>
-                <div className='basis-full lg:basis-10/12 lg:h-screen lg:overflow-y-scroll px-4'>
-                    <h1 className='font-medium text-3xl mt-16 mb-8'>
-                        {t("title")}
-                    </h1>
-                    {
-                        courses.map((course, index) => (
-                            <div key={index}>
-                                <CoursePreview expandedCourseIndex={expandedCourseIndex} bookmarkRef={bookmarkRef} index={index} handleClick={handleCourseClick} key={index} course={course} >
-                                    <div className='mt-auto'>
-                                        <LearnUButton ref={buttonRef} className={'max-md:w-full uppercase'} text={t("Buy")} paddingInline={15} paddingBlock={0} />
-                                    </div>
-                                </CoursePreview>
-                                {/* MOBILE DESIGN STARTS HERE */}
-                                <div className='my-4'>
-                                    {expandedCourseIndex === index && (
-                                        <div className='block lg:hidden bg-primary-white rounded-2xl'>
-                                            <CourseView backgroundImageElement={<CourseViewImage imgUrl={course.imageUrl} />} course={course} >
-                                                {/* Children of the CourseView */}
-                                                <div className='flex flex-col sm:flex-row gap-4 px-8 mt-auto pb-4'>
-                                                    <LearnUButton className={"basis-full uppercase"} text={t("Preview")}   onClick={handlePreviewClick}/>
-                                                    <LearnUButton className={"basis-full uppercase"} text={t("Buy Now")} />
-                                                </div>
-                                            </CourseView>
-                                        </div>
-                                    )}
-                                </div>
-                                {/* MOBILE DESIGN ENDS HERE */}
-                            </div>
-                        ))
-                    }
-
+  return (
+    <main className="w-full md:pl-12 px-[4%] md:px-[2%] lg:px-0">
+      <div className="flex min-w-full gap-4 max-md:pb-20">
+        <div className="basis-full lg:basis-10/12 lg:h-screen lg:overflow-y-scroll px-4">
+          <h1 className="font-medium text-3xl mt-16 mb-8">{t("title")}</h1>
+          {courses.map((course, index) => (
+            <div key={index}>
+              <CoursePreview
+                expandedCourseIndex={expandedCourseIndex}
+                bookmarkRef={bookmarkRef}
+                index={index}
+                handleClick={handleCourseClick}
+                key={index}
+                course={course}
+              >
+                <div className="mt-auto">
+                  <LearnUButton
+                    ref={buttonRef}
+                    className={"max-md:w-full uppercase"}
+                    text={t("Buy")}
+                    paddingInline={15}
+                    paddingBlock={0}
+                  />
                 </div>
-                {/* LAPTOP DESIGN STARTS HERE */}
-                <div className='hidden h-screen lg:block md:basis-full bg-primary-white'>
-                    <div className='flex flex-col h-full p-4'>
-                        {
-                            <CourseView course={selectedCourse}
-                                backgroundImageElement={<CourseViewImage imgUrl={selectedCourse.imageUrl} />}>
-                                {/* Children of the CourseView */}
-                                <div className='flex max-xl:flex-col gap-4 px-8 mt-auto'>
-                                    <LearnUButton className={"basis-full uppercase"} text={t("Preview")} onClick={handlePreviewClick}/>
-                                    <LearnUButton className={"basis-full uppercase"} text={t("Buy Now")} />
-                                </div>
-                            </CourseView>
-                        }
-                    </div>
-                </div>
-                {/* LAPTOP DESIGN ENDS HERE */}
+              </CoursePreview>
+              {/* MOBILE DESIGN STARTS HERE */}
+              <div className="my-4">
+                {expandedCourseIndex === index && (
+                  <div className="block lg:hidden bg-primary-white rounded-2xl">
+                    <CourseView
+                      backgroundImageElement={
+                        <CourseViewImage imgUrl={course.imageUrl} />
+                      }
+                      course={course}
+                    >
+                      {/* Children of the CourseView */}
+                      <div className="flex flex-col sm:flex-row gap-4 px-8 mt-auto pb-4">
+                        <Link href={`/course-detail/${courseIndex}`}>
+                          <LearnUButton
+                            className={"basis-full uppercase"}
+                            text={t("Preview")}
+                          />
+                        </Link>
+                        <LearnUButton
+                          className={"basis-full uppercase"}
+                          text={t("Buy Now")}
+                        />
+                      </div>
+                    </CourseView>
+                  </div>
+                )}
+              </div>
+              {/* MOBILE DESIGN ENDS HERE */}
             </div>
-        </main>
-    )
+          ))}
+        </div>
+        {/* LAPTOP DESIGN STARTS HERE */}
+        <div className="hidden h-screen lg:block md:basis-full bg-primary-white">
+          <div className="flex flex-col h-full p-4">
+            {
+              <CourseView
+                course={selectedCourse}
+                backgroundImageElement={
+                  <CourseViewImage imgUrl={selectedCourse.imageUrl} />
+                }
+              >
+                {/* Children of the CourseView */}
+                <div className="flex max-xl:flex-col gap-4 px-8 mt-auto">
+                  <Link href={`/course-detail/${courseIndex}`}>
+                    <LearnUButton
+                      className={"basis-full uppercase"}
+                      text={t("Preview")}
+                    />
+                  </Link>
+                  <LearnUButton
+                    className={"basis-full uppercase"}
+                    text={t("Buy Now")}
+                  />
+                </div>
+              </CourseView>
+            }
+          </div>
+        </div>
+        {/* LAPTOP DESIGN ENDS HERE */}
+      </div>
+    </main>
+  );
 }
