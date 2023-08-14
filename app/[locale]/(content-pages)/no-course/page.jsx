5f0cc4060f678b56387b87df05d8d6bbb62ed6e3
Tugba Esat <tugbaesat007@gmail.com>
2023-08-14 19:39:13 +0300

    nocoursesfound component created

---
+++ app/[locale]/(content-pages)/no-course/page.jsx
@@ -0,0 +1,8 @@
+import NoCoursesFound from "@/app/Components/NoCoursesFound";
+import React from "react";
+
+const NoCoursePage = () => {
+  return <NoCoursesFound />;
+};
+
+export default NoCoursePage;
