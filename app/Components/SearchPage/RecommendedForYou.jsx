e11ab343fa63a4b0cb52bd7247b2f3c17e898d87
Amdjed Belgacem <45467302+AmdjedBelgacem@users.noreply.github.com>
2023-08-15 23:53:56 +0300

    changed the phone version of search page due to lack of time and a bug, fixed course keyword align, changed sidebar color in the phone version to the one tugba prepared

--- app/Components/SearchPage/RecommendedForYou.jsx
+++ app/Components/SearchPage/RecommendedForYou.jsx
@@ -8,7 +8,7 @@ export default function RecommendedForYou({ courses }) {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const handleNext = () => {
-    if (currentIndex < recommended.length - 1) {
+    if (currentIndex < courses.length - 1) {
       setCurrentIndex(currentIndex + 1);
     }
   };
@@ -38,35 +38,20 @@ export default function RecommendedForYou({ courses }) {
       <h3 className="font-semibold md:text-start">Recommended For You</h3>
       <div className="grid md:grid-cols-4 gap-x-3 w-full">
         {phoneVersion ? (
-          <div className="flex justify-center items-center md:w-4/5 gap-x-3">
-            <button
-              className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-1 px-2 mb-10 rounded-full"
-              onClick={handleBack}
-              disabled={currentIndex === 0}
-            >
-              &#x2190;
-            </button>
-            <div className="w-full flex justify-center max-w-md flex-col items-center ">
-              <div className="rounded-2xl overflow-hidden w-full relative h-[170px] flex justify-center ">
+          courses.map((course, index) => (
+            <div key={index} className="lg:w-full py-3">
+              <div className="rounded-2xl overflow-hidden relative w-full h-[120px]">
                 <Image
-                  src={courses[currentIndex].imageUrl}
+                  src={course.imageUrl}
+                  fill
                   sizes="100vw"
-                  alt="Recommended courses"
+                  alt="courseed courses"
                 />
               </div>
-              <h2 className="font-semibold pt-3">
-                {courses[currentIndex].title}
-              </h2>
-              <p className="text-sm">{courses[currentIndex].tutor.name}</p>
+              <h2 className="font-semibold pt-3">{course.title}</h2>
+              <p className="text-sm">{course.tutor.name}</p>
             </div>
-            <button
-              className="bg-blue-500 hover:bg-blue-600 text-white font-black py-1 px-2 mb-10 rounded-full"
-              onClick={handleNext}
-              disabled={currentIndex === recommended.length - 1}
-            >
-              &#x2192;
-            </button>
-          </div>
+          ))
         ) : (
           courses.map((course, index) => (
             <div key={index} className="lg:w-full py-3">
--- app/Components/SideBar.jsx
+++ app/Components/SideBar.jsx
@@ -24,15 +24,32 @@ function SideBar() {
   const handleSwitchChange = () => {
     setActiveLang((prevLang) => (prevLang === "Türkçe" ? "English" : "Türkçe"));
   };
-  
+
   if (path.includes("tr")) path.replace("/tr", ""); // it will replace the /tr with empty string so that we can use it in the Link component
 
+  const [isLaptop, setIsLaptop] = useState(window.innerWidth >= 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsLaptop(window.innerWidth >= 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   return (
     <>
       <div>
         <div className="flex justify-center md:flex-col md:mt-[4em] md:w-20 md:mr-20">
           <nav className="fixed bottom-2 md:static z-40">
-            <ul className="flex justify-around md:justify-evenly w-[85vw] list-none md:bg-transparent bg-black/75 backdrop-blur-xl p-[1em] rounded-[40px] md:flex-col md:items-center md:h-[25em] md:w-[7em] z-40">
+            <ul
+              className={`flex justify-around md:justify-evenly w-[85vw] list-none ${
+                isLaptop ? "bg-transparent" : "gradient-bg"
+              } p-[1em] rounded-[40px] md:flex-col md:items-center md:h-[25em] md:w-[7em] z-40`}
+            >
               <li className="hidden lg:flex lg:justify-start lg:pb-8">
                 <div className="lg:flex lg:justify-center lg:items-center lg:w-12 lg:h-8 lg:bg-blue-500 lg:rounded-3xl">
                   <Icons.BrandIcon />
@@ -54,7 +71,7 @@ function SideBar() {
                   href="/search"
                   className="mainLinks no-underline text-[#b1b1b1]"
                 >
-                  <Icons.SearchIcon/>
+                  <Icons.SearchIcon />
                   <span className="hidden lg:block lg:text-center lg:text-sm">
                     {t("Search")}
                   </span>
@@ -65,8 +82,10 @@ function SideBar() {
                   href="/my-learning"
                   className="mainLinks no-underline text-[#b1b1b1]"
                 >
-                  <Icons.CoursesIcon/>
-                  <span className="hidden lg:block lg:text-sm">{t("Courses")}</span>
+                  <Icons.CoursesIcon />
+                  <span className="hidden lg:block lg:text-center lg:text-sm">
+                    {t("Courses")}
+                  </span>
                 </Link>
               </li>
               <li>
@@ -74,31 +93,31 @@ function SideBar() {
                   href="/saved"
                   className="mainLinks no-underline text-[#b1b1b1]"
                 >
-                  <Icons.SavedIcon/>
+                  <Icons.SavedIcon />
                   <span className="hidden lg:block lg:text-center lg:text-sm">
                     {t("Saved")}
                   </span>
                 </Link>
               </li>
-              {data && 
+              {data && (
                 <li>
                   <Link
                     href="/profile"
                     className="mainLinks no-underline text-[#b1b1b1]"
                   >
-                    <Icons.ProfileIcon/>
+                    <Icons.ProfileIcon />
                     <span className="hidden lg:block lg:text-center lg:text-sm">
                       {t("Profile")}
                     </span>
                   </Link>
                 </li>
-              }
+              )}
               <li>
                 <Link
                   href="/about"
                   className="mainLinks no-underline text-[#b1b1b1]"
                 >
-                  <Icons.AboutIcon/>
+                  <Icons.AboutIcon />
                   <span className="hidden lg:block lg:text-center lg:text-sm">
                     {t("About")}
                   </span>
@@ -108,8 +127,9 @@ function SideBar() {
                 <div>
                   <Link href={path} locale={locale === "en" ? "tr" : "en"}>
                     <button
-                      className={`rounded-2xl lang-btn text-white ${locale === "en" ? "bg-red-500" : "bg-blue-500"
-                        }`}
+                      className={`rounded-2xl lang-btn text-white ${
+                        locale === "en" ? "bg-red-500" : "bg-blue-500"
+                      }`}
                       onClick={handleSwitchChange}
                     >
                       <div className="primary text">{activeLang}</div>
