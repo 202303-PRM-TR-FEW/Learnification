7564696d2d6614f485bbf0e04f42b63ddfb53053
Furkan Cengiz <muhammedcengiz1@gmail.com>
2023-08-15 14:14:45 +0300

    fix: padding right added

--- app/Components/NoCoursesFound.jsx
+++ app/Components/NoCoursesFound.jsx
@@ -7,7 +7,7 @@ import { useTranslations } from "next-intl";
 const NoCoursesFound = () => {
   const t = useTranslations("NoCoursesFound");
   return (
-    <main className="lg:h-screen lg:flex lg:items-center lg:justify-center lg:my-0 mt-8 mb-20">
+    <main className="lg:h-screen lg:flex lg:items-center lg:justify-center lg:my-0 mt-8 mb-20 lg:pr-8">
       <div className="w-full p-6 shadow-light-gray rounded-[50px] grid lg:grid-cols-2">
         <Icons.NoCourseIcon className="w-full max-lg:max-w-[400px] 2xl:max-h-[600px] 2xl:max-w-[600px] aspect-square" />
         <div className="basis-full my-4 mx-4 text-center lg:text-left lg:pl-4">
