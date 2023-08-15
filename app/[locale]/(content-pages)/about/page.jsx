d4ddd1b66bf49440cd994ce4e0606983632ca742
Ebla Ekerdiker <eblaekerdiker@gmail.com>
2023-08-15 06:56:09 +0300

    Fix: About page top margin added

--- app/[locale]/(content-pages)/about/page.jsx
+++ app/[locale]/(content-pages)/about/page.jsx
@@ -5,7 +5,7 @@ import IntroPic from "@/app/Components/GetStarted/IntroPic";
 export default function About() {
   return (
     <main className="min-h-screen bg-white-smoke mx-4">
-      <div className="w-full shadow-light-gray rounded-[50px] grid lg:grid-cols-2 ">
+      <div className="w-full shadow-light-gray rounded-[50px] grid lg:grid-cols-2 mt-8">
           <IntroPic />
           <AboutUs />
       </div>
